129df24a9f342370b4d77868d6638e1b3f747638
amxr21 <ammar211080@gmail.com>
2025-08-18 19:49:16 +0400

    upload frontend

---
+++ frontend/app/(main)/products/[id]/page.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function product({ params }) {
+  return (
+    <div>product #{params.id}</div>
+  )
+}
---
+++ frontend/app/page.jsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <div className="">
+      <div className="">
+        <h1 className="subheader">test</h1>
+      </div>
+    </div>
+  );
+}
