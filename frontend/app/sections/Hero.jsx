ba851b6682946ce9ee0798ae4b7c8dcb3157c63c
amxr21 <ammar211080@gmail.com>
2025-08-20 14:12:04 +0400

    Update Hero.jsx

--- frontend/app/sections/Hero.jsx
+++ frontend/app/sections/Hero.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
-import { HeroLabel } from '../components';
+import { HeroLabel, ImagesSlider, EmiratiBadge, Stars, Mark } from '../components';
 
 function Hero() {
   return (
-    <div>
+    <div className='relative flex flex-col items-center py-6 mb-20 gap-6'>
         <HeroLabel />
+
+        <EmiratiBadge />
+
+        <div className='relative flex flex-col items-center gap-4 mb-12'>
+          <h1 className='heading'>More than just a Cover</h1>
+          <p className='para italic'>Crafted for iPads, iPhones, and pens — built for your lifestyle.</p>
+          <Mark />
+        </div>
+
+        <Stars />
+        
+
+
+        <ImagesSlider />
+
 
     </div>
   )
