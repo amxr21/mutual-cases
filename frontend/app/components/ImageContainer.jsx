9fe4f9b0a2cc73c4ff2a3142e27829e11b1322ef
amxr21 <ammar211080@gmail.com>
2025-08-20 14:11:39 +0400

    images slider

---
+++ frontend/app/components/ImageContainer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from 'next/image'
+
+
+
+function ImageContainer({ imageSrc, pos }) {
+  let width = 
+   pos == 0 ? '16rem' 
+  :pos == 1 ? '10rem'
+  :pos == 2 ? '15rem'
+  :pos == 3 ? '14rem'
+  :pos == 4 ? '16rem'
+  :pos == 5 ? '10rem'
+  :pos == 6 ? '15rem'
+  :pos == 7 ? '14rem'
+  :pos == 8 ? '16rem'
+  :pos == 9 ? '10rem'
+  :pos == 10 ? '15rem' : '16rem'
+
+
+  return (
+    <div style={{width}} className='relative image-container bg-light-blue rounded-lg h-64 overflow-hidden'>
+        <Image src={imageSrc} alt='' className='absolute inset-0 w-full h-full object-cover' />
+    </div>
+  )
+}
+
+export default ImageContainer
---
+++ frontend/app/components/ImagesSlider.jsx
@@ -0,0 +1,37 @@
+"use client" 
+import { ImageContainer } from './index';
+import { Image1, Image2, Image3, Image4, Image5, Image6 } from "../constants/imags";
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import { Autoplay } from 'swiper/modules';
+ 
+
+function ImagesSlider() {
+  return (
+    <div className='w-full  '>
+      <Swiper
+        autoplay={{ delay: 3000, disableOnInteraction: false }}
+        slidesPerView={'auto'}
+        spaceBetween={16} 
+        height={'10px'}
+        modules={[Autoplay]} 
+        className="mySwiper" 
+      >
+        {
+          [Image1, Image2, Image3, Image4, Image5, Image6, Image2, Image3, Image4, Image5, Image6].map((img, indx) => { 
+            return (
+              <SwiperSlide>
+                <ImageContainer pos={indx} imageSrc={img} />
+              </SwiperSlide>
+            )
+          })
+        }
+
+      </Swiper>
+    </div>
+  )
+}
+
+export default ImagesSlider
