fd5b9a754f05a7c5d77c78eea40f982d72078fb8
amxr21 <ammar211080@gmail.com>
2025-08-20 14:11:15 +0400

    Update HeroLabel.jsx

--- frontend/app/components/HeroLabel.jsx
+++ frontend/app/components/HeroLabel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function HeroLabel() {
   return (
-    <div className='label-large'>Slim. Strong. Yours</div>
+    <div className='label-large bg-blue text-off-white px-2 py-1 rounded-lg w-fit'>Slim. Strong. Yours</div>
   )
 }
 
