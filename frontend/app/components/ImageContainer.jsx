b9f96e378f2535bb9d4aba1d2fc0a74e2d8175a0
amxr21 <ammar211080@gmail.com>
2025-08-21 02:09:39 +0400

    update images slider

--- frontend/app/components/ImageContainer.jsx
+++ frontend/app/components/ImageContainer.jsx
@@ -5,17 +5,17 @@ import Image from 'next/image'
 
 function ImageContainer({ imageSrc, pos }) {
   let width = 
-   pos == 0 ? '16rem' 
+   pos == 0 ? '15.5rem' 
   :pos == 1 ? '10rem'
   :pos == 2 ? '15rem'
   :pos == 3 ? '14rem'
-  :pos == 4 ? '16rem'
+  :pos == 4 ? '15.5rem'
   :pos == 5 ? '10rem'
   :pos == 6 ? '15rem'
   :pos == 7 ? '14rem'
-  :pos == 8 ? '16rem'
+  :pos == 8 ? '15.5rem'
   :pos == 9 ? '10rem'
-  :pos == 10 ? '15rem' : '16rem'
+  :pos == 10 ? '15rem' : '15.5rem'
 
 
   return (
