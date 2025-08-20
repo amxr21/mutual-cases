f5988c957c541c9e7ef1e6df2c62d55e8dd49906
amxr21 <ammar211080@gmail.com>
2025-08-20 14:11:49 +0400

    Create Stars.jsx

---
+++ frontend/app/components/Stars.jsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import { stars } from '../constants/imags'
+
+function Stars() {
+  return (
+    <div className='absolute bottom-0 left-20 z-50'>
+        <Image src={stars} />
+    </div>
+  )
+}
+
+export default Stars
