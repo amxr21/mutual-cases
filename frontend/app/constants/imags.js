bd03f7b7fc10f83e95acbbd0d2f511eabbcfceb0
amxr21 <ammar211080@gmail.com>
2025-08-20 14:11:53 +0400

    Update imags.js

--- frontend/app/constants/imags.js
+++ frontend/app/constants/imags.js
@@ -1,6 +1,15 @@
-import Logo from "../../public/images/logo.png"
+import Logo from "../../public/images/logo.png";
+import Image1 from "../../public/images/products/1.jpg";
+import Image2 from "../../public/images/products/2.jpg";
+import Image3 from "../../public/images/products/3.jpg";
+import Image4 from "../../public/images/products/4.jpg";
+import Image5 from "../../public/images/products/5.jpg";
+import Image6 from "../../public/images/products/6.jpg";
 
+import emiratiBadge from "../../public/images/uae-badge.svg"
 
+import stars from "../../public/images/stars.svg"
 
+import mark from "../../public/images/circle.png"
 
-export { Logo }
+export { Logo, Image1, Image2, Image3, Image4, Image5, Image6, emiratiBadge, stars, mark }
