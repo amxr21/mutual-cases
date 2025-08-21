4f7cb5805a7349c31e3e4838d052326cb9f9d28d
amxr21 <ammar211080@gmail.com>
2025-08-22 02:58:44 +0400

    Update index.js

--- frontend/app/sections/index.js
+++ frontend/app/sections/index.js
@@ -1,4 +1,5 @@
 export { default as Navbar } from "./Navbar.jsx";
 export { default as Hero } from "./Hero.jsx";
 export { default as About } from "./About.jsx";
+export { default as Discover } from "./Discover.jsx";
 export { default as Footer } from "./Footer.jsx";
