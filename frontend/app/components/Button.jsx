e296b5eaeff1d8443df9ba7d6336de24477e66a8
amxr21 <ammar211080@gmail.com>
2025-08-22 02:56:47 +0400

    Create Button.jsx

---
+++ frontend/app/components/Button.jsx
@@ -0,0 +1,11 @@
+"use client"
+
+function Button({buttonContent = '', type = 'text', handleClick, classes}) {
+  return (
+    <button onClick={handleClick} className={`${classes} cursor-pointer text-off-black font-light min-w-4 min-h-4 p-2 bg-off-transparent rounded-md`}>
+        {buttonContent}
+    </button>
+  )
+}
+
+export default Button
