471fa87d41c26ff16398c2d4a0e1e90d998c1e82
amxr21 <ammar211080@gmail.com>
2025-08-22 19:56:36 +0400

    Update AboutText.jsx

--- frontend/app/components/AboutText.jsx
+++ frontend/app/components/AboutText.jsx
@@ -4,22 +4,25 @@ import { BoldText, Header, Mark } from '../components'
 
 function AboutText() {
   return (
-    <div className="about-text">
+    <div className="about-text flex flex-col gap-5">
       <Header headerText='About Mutual' subheader={false} textAlignment='left' mark={true} markClasses='-top-6 left-25 w-42'/>
 
-      <p className='para'>
-          A cover brand for the next generation of thinkers, <BoldText>creators</BoldText>, and <BoldText>doers.</BoldText> Whether you're hustling between classes, sketching at a café, or grinding out ideas at midnight — our covers are designed to move with you.
-      </p>
+      <div>
+        <p className='para'>
+            A cover brand for the next generation of thinkers, <BoldText>creators</BoldText>, and <BoldText>doers.</BoldText> Whether you're hustling between classes, sketching at a café, or grinding out ideas at midnight — our covers are designed to move with you.
+        </p>
 
-      <br />
+        <br />
 
-      <p className="para">
-        Born as the <BoldText>first Emirati brand</BoldText> specializing in iPad covers, we craft premium designs featuring unique phrases and exclusive features — from Apple Pencil charging support to built-in stands for your workflow.
-      </p>
+        <p className="para">
+          Born as the <BoldText>first Emirati brand</BoldText> specializing in iPad covers, we craft premium designs featuring unique phrases and exclusive features — from Apple Pencil charging support to built-in stands for your workflow.
+        </p>
 
-      <br />
+        <br />
 
-      <p className='para'>Our mission is simple: Protect what matters. <BoldText>Do it with style.</BoldText></p>
+        <p className='para'>Our mission is simple: Protect what matters. <BoldText>Do it with style.</BoldText></p>
+      </div>
+
       
     </div>
   )
