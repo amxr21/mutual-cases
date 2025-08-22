9c1e165bca49295bf84e1c6622d345800968a615
amxr21 <ammar211080@gmail.com>
2025-08-22 19:57:22 +0400

    Create Features.jsx

---
+++ frontend/app/sections/Features.jsx
@@ -0,0 +1,36 @@
+import { Header, Feature } from "../components/index"
+
+
+function Features() {
+    
+    const text = {
+        'en': [
+            { 'magnet': ['Multiple positions', 'Detachable into 2 pieces' , 'Hangable on metal surfaces', 'Supports pencil charging', 'Extra protection' ] },
+            { 'simple': ['Combination of 3 pieces', 'Stands in 3 basic positions' , 'Hangable on metal surfaces', 'Supports pencil charging', 'Very light weight' ] },
+            { 'light' : ['Stands in 3 basic positions', 'Supports pencil charging' , 'Light weight', 'Perfect for simple, general use' ] },
+        ]
+    }
+
+
+  return (
+    <div id="Features" className="flex flex-col gap-6 items-center bg-blue py-16 px-20 -mx-20 text-white">
+        <Header textColor="white" textAlignment="center" headerText="We offer three types of cases" subheaderText="Each has specific functions to match your needs!" subheader={true} mark={false} />
+        <ul className="features-details grid grid-cols-3 w-full justify-between">
+            {
+                text['en'].map((feature, indx) => {
+                    let a = Object.entries(feature)[0]
+                    console.log(a);
+                    
+                    return (
+                        <Feature key={indx} index={indx+1} header={a[0]} details={a[1]} />
+                    )
+                })
+                
+
+            }
+        </ul>
+    </div>
+  )
+}
+
+export default Features
