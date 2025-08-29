aebe3cffef5e97edb551ac6a435d10a9eddc67c7
amxr21 <ammar211080@gmail.com>
2025-08-29 22:23:56 +0400

    Create PageBanner.jsx

---
+++ frontend/app/components/PageBanner.jsx
@@ -0,0 +1,26 @@
+"use client"
+
+import Image from "next/image"
+import { Header } from "./index"
+import { Cases } from "../constants/imags"
+
+import { usePathname } from "next/navigation"
+
+
+function PageBanner({ page }) {
+
+  const pathname = usePathname()
+
+  console.log(pathname, 'current route');
+  
+  let currentPath = String(pathname).split("/")[1] == 'products' ? 'Cases' : String(pathname).split("/")[1]
+
+  return (
+    <div className="relative page-banner -mx-20 px-20 py-12 overflow-hidden h-54 flex items-center justify-center mb-10">
+        <Image src={Cases} alt="cases" className="absolute inset-0 w-full h-full object-cover" />
+        <Header headerText={currentPath} subheader={true} subheaderText={`Home / ${currentPath}`} textAlignment="" textColor="white"  />
+    </div>
+  )
+}
+
+export default PageBanner
