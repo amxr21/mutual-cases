13a9eab23b126686752f1c5c0134cf6ae8870fd2
amxr21 <ammar211080@gmail.com>
2025-08-29 22:23:20 +0400

    Update Header.jsx

--- frontend/app/components/Header.jsx
+++ frontend/app/components/Header.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Mark } from './index'
 
-function Header({headerText = "", subheader = true, subheaderText = "", textAlignment = "left", mark = false, markClasses = '', textColor="black"}) {
+function Header({headerText = "", subheader = true, subheaderText = "", textAlignment = "left", mark = false, markClasses = '', textColor="black", extraClasses = ''}) {
   return (
-    <div className={`relative flex flex-col gap-1 ${textAlignment == 'left' ? 'text-left' : 'text-center'}`}>
+    <div className={`relative flex flex-col gap-1 z-50 ${extraClasses} ${textAlignment == 'left' ? 'text-left' : 'text-center'}`}>
         {mark && <Mark classes={markClasses} />}
         <h2 className={`header uppercase text-${textColor}`}>{headerText}</h2>
         {subheader && <p className={`para text-${textColor} ${textAlignment == 'left' ? 'text-left' : 'text-center'}`}>{subheaderText}</p>}
