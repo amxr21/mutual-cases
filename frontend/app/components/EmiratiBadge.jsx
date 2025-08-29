4698018f7f2dec33ec93a753da5fd1109c6bac7c
amxr21 <ammar211080@gmail.com>
2025-08-29 22:22:59 +0400

    Update EmiratiBadge.jsx

--- frontend/app/components/EmiratiBadge.jsx
+++ frontend/app/components/EmiratiBadge.jsx
@@ -5,7 +5,7 @@ import { emiratiBadge } from "../constants/imags"
 
 function EmiratiBadge() {
   return (
-    <div className='absolute top-28 left-72 -rotate-12 z-50'>
+    <div className='absolute top-8 -left-[4rem] -rotate-12 z-50'>
         <Image alt='proudly made by Emirati talents' src={emiratiBadge}  />
     </div>
   )
