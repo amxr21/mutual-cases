595c5d81e0e3d611ec8e827ca5c6c03c185a05af
amxr21 <ammar211080@gmail.com>
2025-08-31 22:42:06 +0400

    Update Button.jsx

--- frontend/app/components/Button.jsx
+++ frontend/app/components/Button.jsx
@@ -2,7 +2,7 @@
 
 function Button({buttonContent = '', type = 'text', handleClick, classes}) {
   return (
-    <button onClick={handleClick} className={`${classes} cursor-pointer text-off-black font-light min-w-4 min-h-4 p-2 bg-off-transparent rounded-md`}>
+    <button onClick={handleClick} className={`${classes} cursor-pointer text-off-black font-light min-w-4 bg-off-transparent rounded-md`}>
         {buttonContent}
     </button>
   )
