0595d921e03d30441e45e003546d2937b3540242
amxr21 <ammar211080@gmail.com>
2025-08-31 22:43:23 +0400

    Create ProductViewContainer.jsx

---
+++ frontend/app/components/ProductViewContainer.jsx
@@ -0,0 +1,10 @@
+
+function ProductViewContainer({ children, classes }) {
+    return (
+        <div className={`${classes} product-view-container -mt-20 p-8  bg-white shadow-2xl rounded-2xl w-full min-h-[20rem] overflow-hidden`}>
+            {children}
+        </div>
+    )
+}
+
+export default ProductViewContainer;
