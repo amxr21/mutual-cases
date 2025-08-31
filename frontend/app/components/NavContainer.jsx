2d49ed12c9b0c58f4dd6225af49e6b5de91ee239
amxr21 <ammar211080@gmail.com>
2025-08-31 22:42:24 +0400

    update nav

--- frontend/app/components/NavContainer.jsx
+++ frontend/app/components/NavContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function NavContainer({children, type}) {
   return (
-    <div className={`bg-off-transparent flex justify-between ${type == 'icon' ? 'w-fit gap-5' : 'gap-14'} px-5 py-4 rounded-lg`}>
+    <div className={`bg-off-transparent flex items-center justify-between ${type == 'icon' ? 'w-fit gap-5' : 'gap-14'} px-5 py-3 rounded-lg`}>
         {children}
     </div>
   )
