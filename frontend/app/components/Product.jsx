8130e82b998aafcb713ba7f608bf8c2cf21abba0
amxr21 <ammar211080@gmail.com>
2025-08-31 22:42:43 +0400

    Update Product.jsx

--- frontend/app/components/Product.jsx
+++ frontend/app/components/Product.jsx
@@ -1,27 +1,15 @@
-import React from 'react'
-import { ProductDetails, ProductImage } from './index'
-
-// {
-//     "id": 10,
-//     "trend": true,
-//     "category": "ipad",
-//     "model": "m1",
-//     "type": "magnet",
-//     "price": 110,
-//     "stock_quantity_id": 46888,
-//     "quantity": 10,
-//     "created_at": "2025-08-17T18:16:39.000Z",
-//     "updated_at": "2025-08-17T18:16:39.000Z"
-//   }
-
+import Link from 'next/link';
+import { ProductCardDetails, ProductImage } from './index';
 
 
 function Product({ details }) {
   return (
-    <div className='w-full flex flex-col gap-3 '>
-        <ProductImage trend={details?.trend} imageUrl = {details?.image_url_1} />
-        <ProductDetails data = { details } />
-    </div>
+    <Link href={`/products/${details.id}`}>
+      <div className='w-full flex flex-col gap-3 '>
+          <ProductImage trend={details?.trend} imageUrl = {details?.image_url_1} />
+          <ProductCardDetails data = { details } />
+      </div>
+    </Link>
   )
 }
 
