1850e2740ca85b32eaad939b0c68032bc294b5bc
amxr21 <ammar211080@gmail.com>
2025-09-13 01:58:56 +0400

    copyrights section mobile view

--- frontend/app/components/CopyRights.jsx
+++ frontend/app/components/CopyRights.jsx
@@ -7,7 +7,7 @@ function CopyRights() {
     <div className='w-full flex flex-col items-center '>
         <Line classes="border-t border-t-gray-400 pt-3 min-w-20 w-2/12" />
 
-        <div className='text-center flex gap-5 justify-center text-sm font-light'>
+        <div className='text-center flex gap-5 justify-center text-xs font-light'>
             <Link href={'mailto:support@mutualcovers'}>support@mutualcovers</Link>
             <p className=''>Established in 2024 - AD, UAE</p>
         </div>
