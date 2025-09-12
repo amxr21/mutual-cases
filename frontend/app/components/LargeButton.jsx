ec355684bfd219ee3e41d7b8d7deaa3f898d3d5d
amxr21 <ammar211080@gmail.com>
2025-09-13 02:05:40 +0400

    Update LargeButton.jsx

--- frontend/app/components/LargeButton.jsx
+++ frontend/app/components/LargeButton.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function LargeButton({ handleClick, text='', classes ='', color='blue' }) {
   return (
-    <button onClick={handleClick} className={`cursor-pointer px-3 py-2 capitalize rounded-xl text-2xl font-semibold ${color == 'blue'? 'bg-blue text-off-white' : 'bg-transparent border border-blue text-blue'} ${classes}`}>
+    <button onClick={handleClick} className={`cursor-pointer px-2 xl:px-3 py-1 xl:py-2 capitalize rounded-xl text-lg xl:text-2xl font-semibold ${color == 'blue'? 'bg-blue text-off-white' : 'bg-transparent border border-blue text-blue'} ${classes}`}>
         {text}
     </button>
   )
