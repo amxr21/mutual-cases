f7363806974dee8604f580a7e0ae6094dd32dce3
amxr21 <ammar211080@gmail.com>
2025-09-13 02:05:50 +0400

    Update Mark.jsx

--- frontend/app/components/Mark.jsx
+++ frontend/app/components/Mark.jsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import { mark } from '../constants/imags'
 
-function Mark({classes = '-top-6 -right-6 '}) {
+function Mark({classes = 'w-36 xl:w-fit top-6 xl:-top-6 xl:-right-6 '}) {
   return (
       <div className={`absolute z-50 ${classes}`}>
           <Image alt='circle with stars' src={mark} />
