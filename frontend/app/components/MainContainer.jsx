564c800a8073fcc2d6fc14ff047f3d22ba730234
amxr21 <ammar211080@gmail.com>
2025-09-13 02:05:46 +0400

    Update MainContainer.jsx

--- frontend/app/components/MainContainer.jsx
+++ frontend/app/components/MainContainer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function MainSection({children, classes, Id}) {
   return (
-    <section id={Id} className={`${classes} flex flex-col gap-10 `}>
+    <section id={Id} className={`flex flex-col xl:gap-10 ${classes}`}>
         {children}
     </section>
   )
