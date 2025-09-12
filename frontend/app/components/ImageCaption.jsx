fab5e48132c7446854772dcc216f6700e0b7ba6c
amxr21 <ammar211080@gmail.com>
2025-09-13 02:05:32 +0400

    Update ImageCaption.jsx

--- frontend/app/components/ImageCaption.jsx
+++ frontend/app/components/ImageCaption.jsx
@@ -4,7 +4,7 @@ import { iPadDrawing, iPhoneDrawing } from "../constants/imags"
 
 function ImageCaption({caseType}) {
   return (
-    <div className={`iphone w-fit text-center flex gap-5 ${caseType == 'iphone' ? 'flex-col' : 'flex-col-reverse'}`}>
+    <div className={`iphone w-fit text-center flex gap-2 xl:gap-5 ${caseType == 'iphone' ? 'flex-col' : 'flex-col-reverse'}`}>
         <Image src={caseType == 'iphone' ? iPhoneDrawing : iPadDrawing} alt={`${caseType == 'iphone' ? 'iPhone' : 'iPad'}`} />
         <p className='font-light italic text-xl'>iPhone cases</p>
     </div>
