5283a971d64df6d5861e54e852e6fa4310a832bb
amxr21 <ammar211080@gmail.com>
2025-09-13 02:05:26 +0400

    Update Filters.jsx

--- frontend/app/components/Filters.jsx
+++ frontend/app/components/Filters.jsx
@@ -14,7 +14,7 @@ let devices =
 
 function Filters() {
   return (
-    <div className='col-span-2 flex flex-col gap-6'>
+    <div className='col-span-0 xl:col-span-2 hidden xl:flex flex-col gap-6'>
 
         <div className='flex pb-2 border-b border-gray-500 h-10 items-center justify-between'>
             <h2 className='font-semibold '>Filters</h2>
