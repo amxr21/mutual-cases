be506a60b64f61efe825ed40cf544ac9f30e047c
amxr21 <ammar211080@gmail.com>
2025-09-13 02:07:42 +0400

    Update Footer.jsx

--- frontend/app/sections/Footer.jsx
+++ frontend/app/sections/Footer.jsx
@@ -3,11 +3,11 @@ import { Brief, Contact, CopyRights, Line, QuickLinks } from '../components'
 
 function Footer() {
   return (
-    <div className='px-20 py-5 flex flex-col gap-10 justify-between items-center'>
+    <div className='px-10 xl:px-20 py-5 flex flex-col gap-5 xl:gap-10 justify-between items-center overflow-hidden'>
         
         <Line classes="border-t border-t-gray-400 pt-2 w-7/12" />
 
-        <div className="flex gap-5 w-full ">
+        <div className="flex flex-col xl:flex-row gap-5 w-full ">
             <Brief />
             <QuickLinks />
             <Contact />
