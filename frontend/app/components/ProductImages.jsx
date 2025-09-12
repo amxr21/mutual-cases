d9b45351228ca112fca9d8eabbf416d8722bd5ca
amxr21 <ammar211080@gmail.com>
2025-09-13 02:26:09 +0400

    Update ProductImages.jsx

--- frontend/app/components/ProductImages.jsx
+++ frontend/app/components/ProductImages.jsx
@@ -2,9 +2,9 @@ import { ImageContainer } from "@/app/components"
 
 function ProductImages({ images }) {
   return (
-    <div className="product-images flex flex-col gap-3 h-full overflow-hidden">
-        <ImageContainer imageSrc={images[0]} classes="grow w-full" pos={'100%'} />
-        <div className="sec-images flex gap-3 w-full h-28 xl:h-40">
+    <div className="product-images flex flex-col gap-2 xl:gap-3 h-full overflow-hidden">
+        <ImageContainer imageSrc={images[0]} classes="grow w-full max-h-48 xl:max-h-full" pos={'100%'} />
+        <div className="sec-images flex gap-2 xl:gap-3 w-full h-24 xl:h-40">
             <ImageContainer imageSrc={images[1]} classes="flex grow h-full" />
             <ImageContainer imageSrc={images[2]} classes="flex grow h-full" />        
         </div>
