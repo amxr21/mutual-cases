5dba3c6ee8ab61c5f68cdc941eb734d68e9aed09
amxr21 <ammar211080@gmail.com>
2025-09-13 02:07:13 +0400

    Update SparkBanner.jsx

--- frontend/app/components/SparkBanner.jsx
+++ frontend/app/components/SparkBanner.jsx
@@ -10,10 +10,10 @@ function SparkBanner() {
         <Image src={Spark} alt='spark' className='' />
         <div className="absolute top-[35%] inset-0 flex flex-col gap-5 h-fit items-center justify-center">
             <div className="text text-center">
-                <h2 className='text-4xl font-semibold'>Got a Design in Mind?</h2>
-                <p className='text-3xl font-light'>Reach us out with your sentance & design</p>
+                <h2 className='text-3xl xl:text-4xl font-semibold'>Got a Design in Mind?</h2>
+                <p className='text-xl xl:text-3xl font-light'>Reach us out with your sentance & design</p>
             </div>
-            <LargeButton handleClick={()=>{}} classes='w-fit px-8' color='blue' text='Enlighten Us' />
+            <LargeButton handleClick={()=>{}} classes='w-fit px-5 xl:px-8' color='blue' text='Enlighten Us' />
         </div>
       </div>
   )
